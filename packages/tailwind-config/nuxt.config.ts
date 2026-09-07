f3f8c646d1ba130e61157e0739a642951e2a211a
zeng <maicaii@vip.qq.com>
2026-09-07 21:51:58 +0800

    重新组织

--- packages/tailwind-config/nuxt.config.ts
+++ packages/tailwind-config/nuxt.config.ts
@@ -1,9 +1,11 @@
+import { fileURLToPath } from 'node:url'
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: [
     '@nuxtjs/tailwindcss'
   ],
+  css: [fileURLToPath(new URL('./src/assets/theme.css', import.meta.url))],
   devtools: { enabled: true },
   compatibilityDate: "latest"
 })
