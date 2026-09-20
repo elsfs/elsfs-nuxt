ec54aaeb0c43b022df3937ad9a4610d7c20c7cb0
zeng <maicaii@vip.qq.com>
2026-09-21 02:20:56 +0800

    优化登录

--- apps/elsfs-admin/app/app.vue
+++ apps/elsfs-admin/app/app.vue
@@ -1,13 +1,10 @@
 <script setup lang="ts">
 import LoadingHide from '~/components/loadingHide.vue'
 import type { AiEmbeddingStore } from 'api-types/ai/aiEmbeddingStore'
-import  {requestPath,useRequestHandle} from 'api-types/ai/aiEmbeddingStore'
-import  { useRequestHandle} from 'api-types/ai/aiEmbeddingStore'
 
-requestPath.getByIdPath
-
-const aiEmbeddingStore = ref<AiEmbeddingStore>()
-
+const  s=reactive<AiEmbeddingStore>({
+  storeId:""
+});
 </script>
 
 <template>
